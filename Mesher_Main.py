ca06b2dc4770d2a6b7acdf9ae40373ec36121032
Christopher Remde <chrissir@live.de>
2018-11-12 18:44:18 +0100

    Update Mesher_Main.py

--- Mesher_Main.py
+++ Mesher_Main.py
@@ -1,8 +1,11 @@
 import os
+from plyfile import PlyData, PlyElement
+import numpy
+
 
 path_to_meshlab_dir: str = "C:\Program Files\VCG\MeshLab\meshlabserver"
 path_to_liveScan3D_files: str = "C:\\Users\\futur\Desktop\BachelorCR\TestSet_Kinect_Recording\\"
-path_to_mesher_script: str = "C:\\Users\\futur\Desktop\BachelorCR\KinectToMesh\meshing.mlx"
+path_to_mesher_script: str = "C:\\Users\\futur\Desktop\BachelorCR\KinectToMesh\mesher.mlx"
 path_to_SOR_script: str = "C:\\Users\\futur\Desktop\BachelorCR\KinectToMesh\SOR.mlx"
 path_to_output: str = "C:\\Users\\futur\Desktop\BachelorCR\KinectToMesh\Mesher\output\\"
 
@@ -30,7 +33,7 @@ def get_number_of_depth_sensors_used(list_of_all_files):
             return previous_depth_sensor_number
 
 
-def meshlabserver_cmd_promt_creator(list_of_all_files, frame_to_process, number_of_frames,input_path, output_path, prefix_string, output_format):
+def meshlabserver_cmd_promt_creator(list_of_all_files, frame_to_process, number_of_frames,input_path, script_path, output_path,  prefix_string, output_format, output_flags):
     input_cmd: str = " -i "
     output_cmd: str = " -o "
     script_cmd: str = " -s "
@@ -46,7 +49,7 @@ def meshlabserver_cmd_promt_creator(list_of_all_files, frame_to_process, number_
     output_file_name = temp_filenametouple[0] + output_format
 
     cmd_promt = path_to_meshlab_dir + meshlabserver_input_cmds + output_cmd + output_path + prefix_string + \
-                output_file_name + script_cmd + path_to_mesher_script
+                output_file_name + output_flags + script_cmd + script_path
     file_output_path = output_path + prefix_string + output_file_name
     print("Final command given to meshlabserver: " + cmd_promt)
     cmd_promt_and_output_path = (cmd_promt, file_output_path)
@@ -68,6 +71,40 @@ def check_dir_make_dir(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+def statistical_outlier_removal(input_file, output_file):
+    remove_all_vertices_above_sd_sigma_of = 1.5  # In Standard Deviation sigma. E.g. 1 means remove all vertices that are
+    #  above 68% of all vertex qualites, 2 means remove all above 95% ect. Defaul is 1.5 or 86%
+
+    pointcloud_with_outliers = PlyData.read(input_file)
+
+    quality_list_with_statistical_outliers = pointcloud_with_outliers['vertex'].data['quality']
+
+    mean = numpy.mean(quality_list_with_statistical_outliers, axis=0)
+    print("The mean of the vertex radius is: " + mean.__str__())
+    sd = numpy.std(quality_list_with_statistical_outliers, axis=0)
+    print("The Standard Deviation of the vertex radius is: " + sd.__str__())
+
+    print("Removing all vertices above sigma of " + remove_all_vertices_above_sd_sigma_of.__str__())
+
+    list_of_vertices_without_SO = []
+
+    for index, x in enumerate(quality_list_with_statistical_outliers):
+        if x < mean + remove_all_vertices_above_sd_sigma_of * sd:
+            list_of_vertices_without_SO.append(pointcloud_with_outliers["vertex"][index])
+
+    print("Removed " + (len(quality_list_with_statistical_outliers) - len(
+        list_of_vertices_without_SO)).__str__() + " outlier vertices")
+    print("Creating PLY file now")
+
+    vertex = numpy.array(list_of_vertices_without_SO,
+                         dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('quality', 'f4')])
+
+    new_ply_element = PlyElement.describe(vertex, "vertex")
+
+    PlyData([new_ply_element]).write(output_file)
+
+    return True;
+
 def main_loop():
 
     # Calculate all static variables
@@ -82,17 +119,19 @@ def main_loop():
     tempdir = path_to_liveScan3D_files + "temp\\"
     check_dir_make_dir(tempdir)
 
-    breakpoint()
     for frame in range(frames_to_process.__int__()):
 
         # Statistic Outlier Removal
-        for kinect in range(number_of_depth_sensors):
+        for index, kinect in enumerate(range(number_of_depth_sensors)):
             cmd_for_meshlabserver_with_output_path = meshlabserver_cmd_promt_creator(list_of_files_to_process,
-                                                                                     current_frame_to_process,
+                                                                                     current_frame_to_process + index,
                                                                                      1,
                                                                                      path_to_liveScan3D_files,
-                                                                                     tempdir, "temp", ".ply")
+                                                                                     path_to_SOR_script,
+                                                                                     tempdir, "temp_with_SO", ".ply",
+                                                                                     " -m vq")
             meshlabserver_supervisor(cmd_for_meshlabserver_with_output_path[0], cmd_for_meshlabserver_with_output_path[1])
+            statistical_outlier_removal(cmd_for_meshlabserver_with_output_path[1], tempdir + "temp_SOR_removed" + (current_frame_to_process + index).__str__() + ".ply")
 
         breakpoint()
         # Meshing
@@ -100,7 +139,8 @@ def main_loop():
                                                                                  current_frame_to_process,
                                                                                  number_of_depth_sensors,
                                                                                  tempdir, path_to_output,
-                                                                                 "meshed", ".obj" )
+                                                                                 path_to_mesher_script,
+                                                                                 "meshed", ".obj", "")
         meshlabserver_supervisor(cmd_for_meshlabserver_with_output_path[0], cmd_for_meshlabserver_with_output_path[1])
         current_frame_to_process += number_of_depth_sensors
     print("No more frames to process! Processed " + current_frame_to_process.__str__() + " frames. Program will exit now")
